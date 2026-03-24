26cd618dbbc26bd957bc8275d6e650cd21f9554b
Ryota <ara.kaki.ryota2000@gmail.com>
2026-03-24 22:45:49 +0900

    add: Dockerfile

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { getUsers } from "../api/api";
-import UserTable from "../components/UserTable";
+import UserList from "../components/UserList";
 
 function UserPage() {
 
@@ -8,34 +8,18 @@ function UserPage() {
 
   useEffect(() => {
 
-    const fetchUsers = async () => {
-      try {
-        const data = await getUsers();
-        setUsers(data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    fetchUsers();
+    getUsers()
+      .then((res) => setUsers(res.data.data.content))
+      .catch((err) => console.error(err));
 
   }, []);
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    window.location.href = "/login";
-  };
 
   return (
     <div>
 
       <h2>User List</h2>
 
-      <button onClick={handleLogout}>
-        Logout
-      </button>
-
-      <UserTable users={users} />
+      <UserList users={users} />
 
     </div>
   );
